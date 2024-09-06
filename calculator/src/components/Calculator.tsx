0ca79c5d06bcea8eaffad5b66ad1c6a5f7b61855
pitzee <pitzee@gmail.com>
2024-09-06 22:10:10 +0300

    css added for the buttons

--- calculator/src/components/Calculator.tsx
+++ calculator/src/components/Calculator.tsx
@@ -7,131 +7,137 @@ const Calculator = () => {
   console.log(numbers);
   return (
     <>
-      <div className="card">
-        <div className="card-body">{numbers}</div>
-      </div>
+      <div className="mx-auto mt-20 bg-slate-400   rounded-md">
+        {/* display */}
+        <div className="span bg-blue-200 border-2 rounded-md h-8">
+          {numbers}
+        </div>
 
-      <div>
-        <button
-          onClick={() => setNumbers((num) => num + "1")}
-          className="btn btn-primary"
-        >
-          1
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "2")}
-          className="btn btn-primary"
-        >
-          2
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "3")}
-          className="btn btn-primary"
-        >
-          3
-        </button>
-        <button
-          onClick={() => setNumbers((sig) => sig + "+")}
-          className="btn btn-primary"
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <button
-          onClick={() => setNumbers((num) => num + "4")}
-          className="btn btn-primary"
-        >
-          4
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "5")}
-          className="btn btn-primary"
-        >
-          5
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "6")}
-          className="btn btn-primary"
-        >
-          6
-        </button>
-        <button
-          onClick={() => setNumbers((sig) => sig + "-")}
-          className="btn btn-primary"
-        >
-          -
-        </button>
-      </div>
-      <div>
-        <button
-          onClick={() => setNumbers((num) => num + "7")}
-          className="btn btn-primary"
-        >
-          7
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "8")}
-          className="btn btn-primary"
-        >
-          8
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "9")}
-          className="btn btn-primary"
-        >
-          9
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "/")}
-          className="btn btn-primary"
-        >
-          /
-        </button>
-      </div>
-      <div>
-        <button
-          onClick={() => setNumbers((num) => num + "0")}
-          className="btn btn-primary"
-        >
-          0
-        </button>
-        <button
-          onClick={() => setNumbers((num) => num + "*")}
-          className="btn btn-primary"
-        >
-          *
-        </button>
-        <button onClick={() => setNumbers("")} className="btn btn-primary">
-          AC
-        </button>
-        <button
-          onClick={() => setNumbers(numbers.slice(0, -1))}
-          className="btn btn-primary"
-        >
-          c
-        </button>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            try {
-              const result = String(eval(numbers));
-              setNumbers(result);
-            } catch (error) {
-              console.error("inavlid expression", error);
-            }
-          }}
-          className="btn btn-primary"
-        >
-          =
-        </button>
-        <button
-          onClick={() => setNumbers((sig) => sig + ".")}
-          className="btn btn-primary"
-        >
-          .
-        </button>
+        <div>
+          <button
+            onClick={() => setNumbers((num) => num + "1")}
+            className="border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            1
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "2")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            2
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "3")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            3
+          </button>
+          <button
+            onClick={() => setNumbers((sig) => sig + "+")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => setNumbers((num) => num + "4")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            4
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "5")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            5
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "6")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            6
+          </button>
+          <button
+            onClick={() => setNumbers((sig) => sig + "-")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            -
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => setNumbers((num) => num + "7")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            7
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "8")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            8
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "9")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            9
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "/")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            /
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => setNumbers((num) => num + "0")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            0
+          </button>
+          <button
+            onClick={() => setNumbers((num) => num + "*")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            *
+          </button>
+          <button
+            onClick={() => setNumbers("")}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            AC
+          </button>
+          <button
+            onClick={() => setNumbers(numbers.slice(0, -1))}
+            className=" border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            c
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              try {
+                const result = String(eval(numbers));
+                setNumbers(result);
+              } catch (error) {
+                console.error("inavlid expression", error);
+              }
+            }}
+            className="border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            =
+          </button>
+          <button
+            onClick={() => setNumbers((sig) => sig + ".")}
+            className="border-2  mt-2 m-2 rounded bg-gray-200 w-10 hover:bg-yellow-100"
+          >
+            .
+          </button>
+        </div>
       </div>
     </>
   );
